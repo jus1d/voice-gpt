9ed0ebaa15b32b8fb8cf10c18c92fa249b5425c2
jus1d <artfa63@gmail.com>
2023-04-27 14:54:47 +0400

    add crc32 hash to fileName & add /voices to .gitignore

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import { message } from 'telegraf/filters';
 import { Telegraf } from "telegraf";
 import config from 'config';
-import { ogg } from './ogg.js'
 import { vocieToText } from './voiceToText.js';
+import crc32 from 'crc32';
 
 const bot = new Telegraf(config.get('TELEGRAM_TOKEN'));
 
@@ -10,9 +10,13 @@ bot.on(message('voice'), async (ctx) => {
     try {
         const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id);
         const userId = String(ctx.message.from.id);
-        const oggFilePath = await vocieToText.createOggFile(link.href, userId);
+        const unixTime = String(Math.floor(Date.now() / 1000));
+        const hash = crc32(unixTime).toString(16);
 
-        vocieToText.createMp3File(userId);
+        const fileName = `${userId}_${hash}`
+        await vocieToText.createOggFile(link.href, fileName);
+
+        vocieToText.createMp3File(fileName);
 
     } catch (error) {
         console.log(error);
