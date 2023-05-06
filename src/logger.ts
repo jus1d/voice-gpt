ab5ef099645811de023f1d4169c2fcfa8329e734
jus1d <artfa63@gmail.com>
2023-05-06 22:00:45 +0400

    add colors to logs

--- src/logger.ts
+++ src/logger.ts
@@ -2,6 +2,17 @@ import dateFormat from 'dateformat';
 import config from 'config';
 import fs from 'fs';
 
+const reset = "\x1b[0m";
+const underscore = "\x1b[4m";
+const dim = "\x1b[2m";
+
+const red = "\x1b[31m";
+const blue = "\x1b[34m";
+const cyan = "\x1b[36m";
+const green = "\x1b[32m";
+const yellow = "\x1b[33m";
+const magenta = "\x1b[35m";
+
 class Logger {
     constructor() {
         try {
@@ -20,24 +31,18 @@ class Logger {
     }
 
     info(message: string, toFile = true) {
-        message = `[${this.getFullDate()}] [  INFO   ] ${message}`;
-
-        console.log(message);
-        if (toFile) this.toFile(message);
+        console.log(`${magenta}[${this.getFullDate()}] ${cyan}[  INFO   ]${reset} ${message}`);
+        if (toFile) this.toFile(`[${this.getFullDate()}] [  INFO   ] ${message}`);
     }
 
     warn(message: string, toFile = true) {
-        message = `[${this.getFullDate()}] [  WARN   ] ${message}`;
-
-        console.log(message);
-        if (toFile) this.toFile(message);
+        console.log(`${magenta}[${this.getFullDate()}] ${yellow}[  WARN   ]${reset} ${message}`);
+        if (toFile) this.toFile(`[${this.getFullDate()}] [  WARN   ] ${message}`);
     }
 
     error(message: string, toFile = true) {
-        message = `[${this.getFullDate()}] [  ERROR  ] ${message}`;
-
-        console.log(message);
-        if (toFile) this.toFile(message);
+        console.log(`${magenta}[${this.getFullDate()}] ${red}[  ERROR  ]${reset} ${message}`);
+        if (toFile) this.toFile(`[${this.getFullDate()}] [  ERROR  ] ${message}`);
     }
 
     start(type: string) {
