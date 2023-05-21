79ec9ac0dcb02fefceb4ea3d3514a63b425f8f7c
jus1d <artfa63@gmail.com>
2023-05-21 11:02:31 +0400

    add other commands

--- srcr/events/conversation.action.ts
+++ srcr/events/conversation.action.ts
@@ -1,13 +1,13 @@
 import { Context, Telegraf } from "telegraf";
 import { Event } from "./event.class";
 
-export class IdCommand extends Event {
+export class ConversationAction extends Event {
     constructor(bot: Telegraf<Context>) {
         super(bot);
     }
 
     handle(): void {
-        this.bot.command('id', async (ctx) => {
+        this.bot.action('get_conversation', async (ctx) => {
             
         });
     }
--- srcr/events/conversation.command.ts
+++ srcr/events/conversation.command.ts
@@ -1,13 +1,17 @@
 import { Context, Telegraf } from "telegraf";
 import { Event } from "./event.class";
 
-export class IdCommand extends Event {
+export class ConversationCommand extends Event {
     constructor(bot: Telegraf<Context>) {
         super(bot);
     }
 
     handle(): void {
-        this.bot.command('id', async (ctx) => {
+        this.bot.command('conversation', async (ctx) => {
+            
+        });
+
+        this.bot.hears(/\/conversation@(\d+)/, async (ctx) => {
             
         });
     }
